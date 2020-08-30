f80204167cd80cfa730a0f8663124ce3ab24bec2
Victor Samuel <victor.samuelsantoss@gmail.com>
2020-08-30 20:14:11 -0300

    att

--- ShellSort.c
+++ ShellSort.c
@@ -1,47 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void shellSort (int *vector, int size);
+void shell_sort (int *v, int tam);
+#define tam 5000
 
 int main (){
 
-    int size, aux = 0;
-	scanf ("%d", &size);
+    int *v = malloc (tam * sizeof (int));
+    int i;
+    for (i = 0; i < tam; i++)
+        v[i] = rand () % 1000;
 
-	int *num = malloc (size * sizeof(int));
+    shell_sort (v, i);
 
-	while (scanf("%d", &num[aux]) != EOF)
-		aux++;
+    for (int i = 0; i < tam; i++)
+        printf ("%d ", v[i]);
 
-	shellSort (num, aux);
-
-    for(int i = 0; i < aux; i++)
-        printf("%d ", num[i]);
-
-    printf ("\n");
-    
     return 0;
 }
 
-void shellSort(int *vet, int size){
+void shell_sort(int *vet, int size){
 
-    int i , j , value;
-    int gap = 1;
+    int i, j, val;
+    int salto = 1;
 
-    while(gap < size)
-        gap = 3*gap+1;
+    while(salto < size)
+        salto = 3*salto+1;
 
-    while (gap > 1){
-        gap /= 3;
+    while (salto > 1){
+        salto /= 3;
 
-        for(i = gap; i < size; i++){
-            value = vet[i];
-            j = i-gap;
-            while (j >= 0 && value < vet[j]){
-                vet [j+gap] = vet[j];
-                j -= gap;
+        for(i = salto; i < size; i++){
+            val = vet[i];
+            j = i-salto;
+            while (j >= 0 && val < vet[j]){
+                vet [j+salto] = vet[j];
+                j -= salto;
             }
-            vet [j+gap] = value;
+            vet [j+salto] = val;
         }
     }
 }
